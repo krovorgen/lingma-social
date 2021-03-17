f4d18c423045ca5150a88c7146157f6342a60ddc
Максим Сергеевич <krovorgen@gmail.com>
2021-03-17 23:08:40 +0300

    Added ProfileInfo components

---
+++ src/components/Profile/ProfileInfo/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styles from "./style.module.scss";
+
+const ProfileInfo = () => {
+    return (
+        <>
+            <div className={styles["main-page__images"]}>
+                <img src="img/main-bg.jpg" alt="" className={styles["main-page__img"]}/>
+            </div>
+            <div>ava + description</div>
+        </>
+    )
+}
+
+export default ProfileInfo;
--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 
-import styles from "./style.module.scss";
 import MyPosts from "./MyPosts";
+import ProfileInfo from "./ProfileInfo";
+
+import styles from "./style.module.scss";
 
 const Profile = () => {
     return (
         <div className={styles["main-page"]}>
-            <div className={styles["main-page__images"]}>
-                <img src="img/main-bg.jpg" alt="" className={styles["main-page__img"]}/>
-            </div>
-            <div className={styles["main-page__container"]}>
-                <div>ava + description</div>
-                <MyPosts/>
-            </div>
+            <ProfileInfo/>
+            <MyPosts/>
         </div>
     )
 }
