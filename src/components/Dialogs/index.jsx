1e7a1365fd417a6f96b5cee547db674be92c1de7
Максим Сергеевич <krovorgen@gmail.com>
2021-03-18 23:34:17 +0300

    Added BLL

--- src/components/Dialogs/index.jsx
+++ src/components/Dialogs/index.jsx
@@ -6,15 +6,30 @@ import MessageItem from "../MessageItem";
 import styles from "./style.module.scss";
 
 const Dialogs = () => {
+
+    let dialogsData = [
+        {id: 1, name: "Maksim"},
+        {id: 2, name: "Sergey"},
+        {id: 3, name: "Vadim"},
+    ];
+
+    let messagesData = [
+        {id: 1, message: "Hello, world!"},
+        {id: 2, message: "Привет, мир!"},
+        {id: 3, message: "Hallo, Welt!"},
+    ];
+
     return (
         <div className={styles["dialogs"]}>
             <div className={styles["dialog-items"]}>
-                <DialogItem name="Maksim" id="1"/>
-                <DialogItem name="Sergey" id="2"/>
-                <DialogItem name="Vadim" id="3"/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
             </div>
             <div className={styles["message-items"]}>
-                <MessageItem message="Hello world"/>
+                <MessageItem message={messagesData[0].message}/>
+                <MessageItem message={messagesData[1].message}/>
+                <MessageItem message={messagesData[2].message}/>
             </div>
         </div>
     )
--- src/components/MyPosts/index.jsx
+++ src/components/MyPosts/index.jsx
@@ -5,15 +5,21 @@ import Post from "../Post";
 
 const MyPosts = () => {
 
+    let postsData = [
+        {id: 1, message: "Hello maks", like: "1"},
+        {id: 2, message: "Hi", like: "2"},
+        {id: 3, message: "", like: ""},
+    ];
+
     return (
         <div className={styles["my-posts"]}>My posts
             <form className={styles["my-posts__form"]} action="#">
                 <textarea className={styles["my-posts__textarea"]} name="" placeholder="Write your message"/>
                 <button className={styles["my-posts__button"]} type="submit">Add post</button>
             </form>
-            <Post message="Hello maks" like="1"/>
-            <Post message="Hi" like="2"/>
-            <Post />
+            <Post message={postsData[0].message} like={postsData[0].like}/>
+            <Post message={postsData[1].message} like={postsData[1].like}/>
+            <Post message={postsData[2].message} like={postsData[2].like}/>
         </div>
     )
 }
