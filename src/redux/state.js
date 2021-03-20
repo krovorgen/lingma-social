e35e5ca682c755ce18f7c90a7e4987d02e59c594
Максим Сергеевич <krovorgen@gmail.com>
2021-03-21 00:40:10 +0300

    Added state.js

---
+++ src/redux/state.js
@@ -0,0 +1,24 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: "Hello maks", like: "1"},
+            {id: 2, message: "Hi", like: "2"},
+            {id: 3, message: "", like: ""},
+        ],
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: "Maksim"},
+            {id: 2, name: "Sergey"},
+            {id: 3, name: "Vadim"},
+        ],
+        messages: [
+            {id: 1, message: "Hello, world!"},
+            {id: 2, message: "Привет, мир!"},
+            {id: 3, message: "Hallo, Welt!"},
+        ],
+    },
+};
+
+
+export default state;
