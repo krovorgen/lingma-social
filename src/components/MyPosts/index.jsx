2108ffe85ec9accf95f13bbb0b7fa348d980dbf4
Максим Сергеевич <krovorgen@gmail.com>
2021-03-21 17:43:17 +0300

    added event listener on textarea and button

--- src/components/MyPosts/index.jsx
+++ src/components/MyPosts/index.jsx
@@ -10,11 +10,17 @@ const MyPosts = ({
 
     let postsElements = posts.map(p => <Post message={p.message} like={p.like}/>)
 
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+    }
+
     return (
         <div className={styles["my-posts"]}>My posts
             <form className={styles["my-posts__form"]} action="#">
-                <textarea className={styles["my-posts__textarea"]} name="" placeholder="Write your message"/>
-                <button className={styles["my-posts__button"]} type="submit">Add post</button>
+                <textarea className={styles["my-posts__textarea"]} ref={newPostElement} placeholder="Write your message"/>
+                <button className={styles["my-posts__button"]} onClick={addPost}>Add post</button>
             </form>
             {postsElements}
         </div>
