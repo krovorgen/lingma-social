f944e0b0195c6ff8d4b4246258dae541b986b8c5
Максим Сергеевич <krovorgen@gmail.com>
2021-04-04 10:36:04 +0300

    Added axios and get request

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as axios from 'axios';
 
 import styles from './style.module.scss';
 
@@ -8,35 +9,23 @@ const Users = ({
     unfollow,
     setUsers,
 }) => {
-    if (users.length === 0) {
-        setUsers([
-            {
-                id: 1,
-                followed: true,
-                firstName: 'Maksim',
-                lastName: 'Biarozka',
-                status: 'Hello world',
-                location: { city: 'Minsk', country: 'Belarus' },
-            },
-            {
-                id: 2,
-                followed: false,
-                firstName: 'Test',
-                lastName: 'Users',
-                status: 'Hi bitches',
-                location: { city: 'Lida', country: 'Belarus' },
-            },
-        ]);
-    }
+    const getUsers = () => {
+        if (users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
+                setUsers(response.data.items);
+            });
+        }
+    };
 
     return (
         <div className={styles['users-items']}>
+            <button type="submit" onClick={getUsers}>GetUsers</button>
             {
                 users.map((u) => (
                     <div key={u.id} className={styles['users-item']}>
                         <div className={styles['users-item__follow']}>
                             <div className={styles['users-item__follow-images']}>
-                                <img src={`/img/users/${u.id}.jpg`} alt="" />
+                                <img src={u.photos.small || '/img/users/1.jpg'} alt="" />
                             </div>
                             {u.followed
                                 ? <button className={styles['users-item__follow-btn']} onClick={() => { unfollow(u.id); }} type="submit">Unsubscribe</button>
@@ -44,15 +33,13 @@ const Users = ({
                         </div>
                         <div className={styles['users-item__info']}>
                             <div className={styles['users-item__info-name']}>
-                                {u.firstName}
-                                {' '}
-                                {u.lastName}
+                                {u.name}
                             </div>
                             <div className={styles['users-item__info-status']}>{u.status}</div>
                             <div className={styles['users-item__info-location']}>
-                                {u.location.city}
-                                {' '}
-                                {u.location.country}
+                                {/* {u.location.city} */}
+                                {/* {' '} */}
+                                {/* {u.location.country} */}
                             </div>
                         </div>
                     </div>
